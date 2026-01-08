a168e09f9dc9123c962693124604400dab44af32
Eslam Soliman <eslamveteo@gmail.com>
2026-01-08 22:16:29 +0200

    Auto-generated commit

---
+++ chunk-DUBLS5BN.js
@@ -0,0 +1,1 @@
+import{r as a}from"./chunk-Q23L6LWU.js";import{X as r,aa as o}from"./chunk-ZPGQN5EY.js";var c=class e{http=o(a);getUserCertificates(t,i){return this.http.get(`/api/Certificate/${i}/${t}`)}createCourseCertificate(t){return this.http.post("/api/Certificate",t)}static \u0275fac=function(i){return new(i||e)};static \u0275prov=r({token:e,factory:e.\u0275fac})};export{c as a};
--- chunk-QA5BRCNH.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./chunk-E7G3M2PQ.js";import{X as r,aa as o}from"./chunk-2ZBRTNB4.js";var c=class e{http=o(a);getUserCertificates(t,i){return this.http.get(`/api/Certificate/${i}/${t}`)}createCourseCertificate(t){return this.http.post("/api/Certificate",t)}static \u0275fac=function(i){return new(i||e)};static \u0275prov=r({token:e,factory:e.\u0275fac})};export{c as a};
